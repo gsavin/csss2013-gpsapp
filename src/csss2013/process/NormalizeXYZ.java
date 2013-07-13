37b6ad707a9cfe23f26cb9d0c8d4d1ced8909597
gsavin <guilhelm.savin@graphstream-project.org>
2013-07-13 14:32:51 +0200

    ..

--- src/csss2013/process/NormalizeXYZ.java
+++ src/csss2013/process/NormalizeXYZ.java
@@ -6,15 +6,22 @@
  */
 package csss2013.process;
 
+import org.graphstream.graph.Node;
+
 import csss2013.App;
 import csss2013.Process;
 import csss2013.Trace;
 import csss2013.annotation.Default;
 import csss2013.annotation.Title;
+import csss2013.util.Tools;
 
 @Default
 @Title("Normalize XYZ")
 public class NormalizeXYZ implements Process {
+	public static enum Type {
+		NORMALIZE, PROJECTION
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -30,6 +37,19 @@ public class NormalizeXYZ implements Process {
 	 * @see csss2013.Process#process(csss2013.App)
 	 */
 	public void process(App app) {
+		Type type = Type.PROJECTION;
+
+		switch (type) {
+		case NORMALIZE:
+			normalize(app);
+			break;
+		default:
+			projection(app);
+			break;
+		}
+	}
+
+	public void normalize(App app) {
 		double x = 0, y = 0, n = 0;
 
 		for (int i = 0; i < app.getTraceCount(); i++) {
@@ -56,6 +76,63 @@ public class NormalizeXYZ implements Process {
 				xyz[1] -= y;
 
 				t.getNode(j).setAttribute("xyz", xyz);
+			}
+		}
+	}
+
+	public void projection(App app) {
+		double minLat, maxLat, minLon, maxLon;
+
+		minLat = minLon = Double.MAX_VALUE;
+		maxLat = maxLon = Double.MIN_VALUE;
+
+		for (int i = 0; i < app.getTraceCount(); i++) {
+			Trace t = app.getTrace(i);
+
+			for (int j = 0; j < t.getNodeCount(); j++) {
+				Node n = t.getNode(j);
+
+				double lon, lat;
+				lon = n.getNumber("lon");
+				lat = n.getNumber("lat");
+
+				minLat = Math.min(minLat, lat);
+				maxLat = Math.max(maxLat, lat);
+				minLon = Math.min(minLon, lon);
+				maxLon = Math.max(maxLon, lon);
+			}
+		}
+
+		double cLon, cLat;
+		cLon = (minLon + maxLon) / 2.0;
+		cLat = (minLat + maxLat) / 2.0;
+
+		System.out.printf("Center (%f;%f)\n", cLon, cLat);
+		for (int i = 0; i < app.getTraceCount(); i++) {
+			Trace t = app.getTrace(i);
+
+			for (int j = 0; j < t.getNodeCount(); j++) {
+				Node n = t.getNode(j);
+
+				double lon, lat;
+				lon = n.getNumber("lon");
+				lat = n.getNumber("lat");
+
+				double x = Tools.distance(cLat, lon, cLat, cLon);
+				double y = Tools.distance(lat, cLon, cLat, cLon);
+
+				if (lon < cLon)
+					x *= -1;
+
+				if (lat < cLat)
+					y *= -1;
+
+				double[] xyz = n.getAttribute("xyz");
+				xyz[0] = x;
+				xyz[1] = y;
+				xyz[2] = 0;
+
+				n.setAttribute("xyz", xyz);
 			}
 		}
 	}
--- src/csss2013/util/Tools.java
+++ src/csss2013/util/Tools.java
@@ -1,6 +1,7 @@
 package csss2013.util;
 
 import java.util.Calendar;
+import java.util.Locale;
 
 import org.graphstream.graph.Node;
 
@@ -12,6 +13,16 @@ public class Tools {
 		double lat1 = n1.getNumber("lat"), lat2 = n2.getNumber("lat"), lon1 = n1
 				.getNumber("lon"), lon2 = n2.getNumber("lon");
 
+		return distance(lat1, lon1, lat2, lon2);
+	}
+
+	public static double distance(double lat1, double lon1, double lat2,
+			double lon2) {
+		return 1852.0 * orthodromie(lon1, lat1, lon2, lat2);
+	}
+
+	public static double distance__(double lat1, double lon1, double lat2,
+			double lon2) {
 		double pk = (double) (180 / Math.PI);
 
 		double a1 = lat1 / pk;
@@ -27,11 +38,36 @@ public class Tools {
 		return 6366000 * tt;
 	}
 
+	public static double orthodromie(double lat1, double lon1, double lat2,
+			double lon2) {
+		lon1 = Math.toRadians(lon1);
+		lon2 = Math.toRadians(lon2);
+		lat1 = Math.toRadians(lat1);
+		lat2 = Math.toRadians(lat2);
+
+		double d = Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1)
+				* Math.cos(lat2) * Math.cos(lon2 - lon1);
+		d = 60 * Math.toDegrees(Math.acos(d));
+
+		return d;
+	}
+
 	public static synchronized Calendar getCalendarTime(Node n) {
 		return n.getAttribute("time.calendar");
 	}
 
 	public static long getTime(Node n) {
 		return n.getAttribute("time.ms");
+	}
+
+	public static void main(String... args) {
+		// (48°51' N, 2°21' E)--(40°43'N, 74°00'W) : 5830 km.
+		double parisLat = 48.856578;
+		double parisLon = 2.351828;
+		double nyLat = 40.713361;
+		double nyLon = -74.005594;
+		double d = distance__(parisLat, parisLon, nyLat, nyLon);
+		double o = distance(parisLat, parisLon, nyLat, nyLon);
+		System.out.printf(Locale.ROOT, "%.2f |%.2f\n", o, d);
 	}
 }
